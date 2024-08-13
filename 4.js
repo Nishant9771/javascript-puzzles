3f97d40f19a5efba6a41a2528b1c73f77e6d29c9
Nishant Kumar <nishantparmar8298@gmail.com>
2024-08-14 02:33:12 +0530

    Update 4.js
    The issue in these code is that the if statement is not properly enclosing the line.
    
    Without curly braces {}, only the first statement after the if condition is considered part of the if block.
    
    By adding curly braces around the code inside the if statement, we ensure that both console.log and evenNumbers.push are executed only when the condition num % 2 === 0 is true.
    
    Thank You!

--- 4.js
+++ 4.js
@@ -1,19 +1,22 @@
-// problem: it is adding all of the numbers to the evenNumbers array
-// even though it should only add even numbers
-// fix it to only add even numbers!
-
-var evenNumbers = []
+var evenNumbers = [];
 
 function addEvensOnly(num) {
-  if (num % 2 === 0)
-    console.log(num, 'is even, adding!')
-    evenNumbers.push(num)
+  if (num % 2 === 0) {
+    console.log(num, 'is even, adding!');
+    evenNumbers.push(num);
+  }
 }
 
-addEvensOnly(0)
-addEvensOnly(1)
-addEvensOnly(2)
-addEvensOnly(3)
-addEvensOnly(4)
+//The issue in these code is that the if statement is not properly enclosing the line.
+//Without curly braces {}, only the first statement after the if condition is considered part of the if block.
+// By adding curly braces around the code inside the if statement, we ensure that both console.log and evenNumbers.push are executed only when the condition num % 2 === 0 is true.
 
-console.log('even numbers: ', evenNumbers)
+
+addEvensOnly(0);
+addEvensOnly(1);
+addEvensOnly(2);
+addEvensOnly(3);
+addEvensOnly(4);
+
+console.log('even numbers: ', evenNumbers);
+
